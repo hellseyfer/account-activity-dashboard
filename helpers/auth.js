e10ca1849e5b553767f26a441c6ee61667ef90ff
Juan Montilla <juan.montilla@outlook.com.ar>
2020-06-27 00:44:36 -0300

    fix dotenv

--- helpers/auth.js
+++ helpers/auth.js
@@ -4,7 +4,7 @@ const passport = require('passport')
 const TwitterStrategy = require('passport-twitter')
 const httpAuth = require('http-auth')
 
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 
 var auth = {}
 
